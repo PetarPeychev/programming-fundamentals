95d6d5a1ef4050cb29f1b03a9831c42100091934
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-11-10 14:19:29 +0200

    Create 05-character-stats.cs

---
+++ csharp-intro-and-basic-syntax-exercises/05-character-stats.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Console_.NET_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maximumHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maximumEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Health: |{0}{1}|", 
+                new string('|', currentHealth), new string('.', maximumHealth - currentHealth));
+            Console.WriteLine("Energy: |{0}{1}|",
+                new string('|', currentEnergy), new string('.', maximumEnergy - currentEnergy));
+        }
+    }
+}
