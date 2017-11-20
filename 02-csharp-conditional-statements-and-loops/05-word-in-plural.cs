a296e78a23bd3ba6db61bc0db7d750f846fe4add
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-11-20 16:19:30 +0200

    Create 05-word-in-plural.cs

---
+++ 02-csharp-conditional-statements-and-loops/05-word-in-plural.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Console_.NET_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string noun = Console.ReadLine();
+            int nounLength = noun.Length;
+            string plural;
+
+            if (noun.EndsWith("y"))
+            {
+                plural = noun.Remove(nounLength - 1);
+                plural = plural + "ies";
+            }
+            else if (noun.EndsWith("o") ||
+                noun.EndsWith("ch") ||
+                noun.EndsWith("s") ||
+                noun.EndsWith("sh") ||
+                noun.EndsWith("x") ||
+                noun.EndsWith("z"))
+            {
+                plural = noun + "es";
+            }
+            else
+            {
+                plural = noun + "s";
+            }
+
+            Console.WriteLine(plural);
+        }
+    }
+}
