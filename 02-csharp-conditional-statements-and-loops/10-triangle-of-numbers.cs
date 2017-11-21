6e3bd969341913f3b194a812c4a1acbe682b3b1c
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-11-21 17:36:03 +0200

    Create 10-triangle-of-numbers.cs

---
+++ 02-csharp-conditional-statements-and-loops/10-triangle-of-numbers.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Console_.NET_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
