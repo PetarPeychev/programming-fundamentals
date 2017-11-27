3df9b2751bdd1b3830df77c74d436132f60ae5a5
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-11-27 20:21:08 +0200

    Create 03-practice-chars-and-strings.cs

---
+++ 03-data-types-and-variables-exercises/03-practice-chars-and-strings.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ConsoleNETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstInput = "Software University";
+            char secondInput = 'B';
+            char thirdInput = 'y';
+            char fourthInput = 'e';
+            string fifthInput = "I love programming";
+
+            Console.WriteLine(firstInput);
+            Console.WriteLine(secondInput);
+            Console.WriteLine(thirdInput);
+            Console.WriteLine(fourthInput);
+            Console.WriteLine(fifthInput);
+        }
+    }
+}
