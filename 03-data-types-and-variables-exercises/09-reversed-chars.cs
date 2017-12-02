45c8133cf7f75c7f85355ec3cb74eff6de8007d5
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2017-12-02 14:20:47 +0200

    Create 09-reversed-chars.cs

---
+++ 03-data-types-and-variables-exercises/09-reversed-chars.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleNETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{thirdChar}{secondChar}{firstChar}");
+        }
+    }
+}
