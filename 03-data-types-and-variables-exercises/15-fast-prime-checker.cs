9243ba0db6deceb8c3a99044e5bd769a79c0bec7
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2018-01-04 11:57:27 +0200

    Create 15-fast-prime-checker.cs

---
+++ 03-data-types-and-variables-exercises/15-fast-prime-checker.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace ConsoleNETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= range; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isPrime}");
+            }
+        }
+    }
+}
