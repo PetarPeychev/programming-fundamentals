df21cae65304fd8902e41b761fdbeb9937bc1b27
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2018-01-05 14:07:19 +0200

    Update 19-thea-the-photographer.cs

--- 03-data-types-and-variables-exercises/19-thea-the-photographer.cs
+++ 03-data-types-and-variables-exercises/19-thea-the-photographer.cs
@@ -6,15 +6,13 @@ namespace ConsoleNETCore
     {
         static void Main(string[] args)
         {
-            // Only 70/100 points. No idea what's going wrong.
-            
             int totalPicturesTaken = int.Parse(Console.ReadLine());
             int filterTimeSeconds = int.Parse(Console.ReadLine());
-            byte filterFactor = byte.Parse(Console.ReadLine());
+            int filterFactor = int.Parse(Console.ReadLine());
             int uploadTimeSeconds = int.Parse(Console.ReadLine());
             int filteredPictures = (int)Math.Ceiling(((double)totalPicturesTaken * filterFactor) / 100);
-            int totalFilterTimeSeconds = totalPicturesTaken * filterTimeSeconds;
-            int totalUploadTimeSeconds = filteredPictures * uploadTimeSeconds;
+            long totalFilterTimeSeconds = (long)totalPicturesTaken * filterTimeSeconds;
+            long totalUploadTimeSeconds = (long)filteredPictures * uploadTimeSeconds;
             TimeSpan totalTime = TimeSpan.FromSeconds(totalUploadTimeSeconds + totalFilterTimeSeconds);
             string totalTimeFormatted = totalTime.ToString(@"d\:hh\:mm\:ss");
             Console.WriteLine(totalTimeFormatted);
