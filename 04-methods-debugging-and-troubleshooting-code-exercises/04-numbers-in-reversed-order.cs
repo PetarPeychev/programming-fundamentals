0fd031e23c282baa59b9bd6f3aab570674db3c18
Petar Peychev <33529197+PetarPeychev@users.noreply.github.com>
2018-01-23 09:31:53 +0200

    Create 04-numbers-in-reversed-order.cs

---
+++ 04-methods-debugging-and-troubleshooting-code-exercises/04-numbers-in-reversed-order.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ConsoleNETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal inputNumber = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(ReverseDecimal(inputNumber));
+        }
+
+        static decimal ReverseDecimal(decimal input)
+        {
+            string stringRepresentation = input.ToString();
+            string reversedString = "";
+            for (int i = stringRepresentation.Length - 1; i >= 0; i--)
+            {
+                reversedString += stringRepresentation[i];
+            }
+            return decimal.Parse(reversedString);
+        }
+    }
+}
